4c1c63610a8729dbae88c1b0d14c8c3e62e4a403
Richard <richard.yang.tong@gmail.com>
2021-03-01 13:40:13 -0500

    Updatad the schema for user_info table.

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,15 @@
-CREATE DATABASE IF NOT EXISTS fluxim;
-USE fluxim;
+DROP DATABASE IF EXISTS flux_db;
 
-CREATE TABLE userinfo (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255),
-  password VARCHAR(255),
-  accessKey VARCHAR(255),
-  isadmin BOOLEAN
-)
+CREATE DATABASE flux_db;
+
+USE flux_db;
+
+CREATE TABLE user_info (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  user_email VARCHAR(255) NOT NULL,
+  user_password VARCHAR(255) NOT NULL,
+  access_key VARCHAR(255),
+  isAdmin BOOLEAN DEFAULT false,
+  PRIMARY KEY(id)
+);
 
