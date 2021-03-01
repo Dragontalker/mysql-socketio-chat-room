520ed46f1d1ff82eb6eccf81cd2cc35a31061719
jcxiao <chang.x1994@gmail.com>
2021-03-01 14:19:17 -0500

    Added functionality to chatroom page

--- public/assets/JavaScript/chatroom.js
+++ public/assets/JavaScript/chatroom.js
@@ -1,11 +1,14 @@
 const socket = io();
 
+document.querySelector('#msgForm').addEventListener('click', sendMsg);
+
+
 // send message to server
 function sendMsg(e) {
   e.preventDefault();
   const msg = document.querySelector('#msg').value;
   if (msg) {
-    socket.emit('message', {user:username, msg:msg, id:socket.id});
+    socket.emit('message', {user:'User1', msg:msg, id:socket.id});
     document.querySelector('#msg').value = '';
   }
 }
