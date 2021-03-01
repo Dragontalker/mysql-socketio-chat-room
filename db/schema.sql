9617ea602aaa6bea41f2f706c910d14b8ca4d531
Richard Yang <richard.yang.tong@gmail.com>
2021-03-01 14:27:16 -0500

    Update from development branch @ 2021-03-01 2:26pm (#9)
    * Added schema for login info
    
    * fixed typo in schema
    
    * fixed another typo in schema
    
    * separated server into routing files
    
    * added .env and package-lock.json in .gitignore, and added .eslintignore
    
    * Added test changes to index.html
    
    * html page
    
    * noaccess.html
    
    * Testing development branch.
    
    * Added a connection module for MySQL database.
    
    * Added a custome ORM.
    
    * noaccess.html
    
    * Updatad the schema for user_info table.
    
    * Blocked out chatroom pieces
    
    * Added the seeds file for MySQL database.
    
    * Renamed user_info into login_info in schema.
    
    * Added a new schema for user_info table.
    
    * Made changes to index page and linked to sign up
    
    * Added more seeds for user_info table.
    
    * Added a new table user_messages to schema.
    
    * Changed channel_name into channel_id in table user_messages.
    
    * Made changes to seeds to be consistent with change made to schema.
    
    * Added a new table channels to schema file.
    
    * Added basic parts of chatroom page
    
    * Added more seeds for table channels.
    
    * Added functionality to chatroom page
    
    * avatar images
    
    * Updated the comments in seeds.
    
    Co-authored-by: jcxiao <chang.x1994@gmail.com>
    Co-authored-by: Sam Kim <sendtosamkim@gmail.com>
    Co-authored-by: Michael Wong <michael.w228@gmail.com>

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,39 @@
-CREATE DATABASE IF NOT EXISTS fluxim;
-USE fluxim;
+DROP DATABASE IF EXISTS flux_db;
 
-CREATE TABLE userinfo (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255),
-  password VARCHAR(255),
-  accessKey VARCHAR(255),
-  isadmin BOOLEAN
-)
+CREATE DATABASE flux_db;
+
+USE flux_db;
+
+CREATE TABLE login_info (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  user_email VARCHAR(255) NOT NULL,
+  user_password VARCHAR(255) NOT NULL,
+  access_key VARCHAR(255),
+  isAdmin BOOLEAN DEFAULT false,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  login_id INTEGER NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  display_name VARCHAR(30),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE messages (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  login_id INTEGER NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  display_name VARCHAR(30),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE channels (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  channel_name VARCHAR(50),
+  PRIMARY KEY(id)
+);
 
