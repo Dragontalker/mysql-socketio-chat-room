22fcb01db148f1317b7503fe91e59c4394651065
jcxiao <chang.x1994@gmail.com>
2021-03-01 10:34:55 -0500

    Added basic code for socketIO functionality to server

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 const express = require('express');
-
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 3000;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.listen(PORT, () => {
-    console.log(`Server listening at port ${PORT}...`);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    console.log('GET REQUEST: index');
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/chatroom', (req, res) => {
+    res.sendFile(__dirname + '/public/chatroom.html');
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on('message', (data) => {
+        console.log(`message from ${data.user}: ${data.msg}`);
+        io.emit('receivedMsg', { user: data.user, msg: data.msg, id: socket.id });
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`user ${socket.id} disconnected`);
+    })
+});
+
+http.listen(PORT, () => {
+    console.log('listening on port', PORT);
 });
