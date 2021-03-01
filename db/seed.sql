10c7bb07fc0056e1ab09150f1c8a4baee5c2a59f
Richard <richard.yang.tong@gmail.com>
2021-03-01 14:03:19 -0500

    Added more seeds for user_info table.

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,4 @@
+-- For table: login_info
 INSERT INTO login_info (user_email, user_password, isAdmin) 
 VALUES ('chang.xiao@flux.com','adminPassword', true);
 
@@ -9,3 +10,17 @@ VALUES ('micheal.wong@flux.com','guessPassword2');
 
 INSERT INTO login_info (user_email, user_password) 
 VALUES ('richard.yang@flux.com','guestPassword3');
+
+
+-- For tablee: user_info
+INSERT INTO user_info (login_id, first_name, last_name, display_name)
+VALUES (1, 'Chang', 'Xiao', 'Adminstrator');
+
+INSERT INTO user_info (login_id, first_name, last_name, display_name)
+VALUES (2, 'Sam', 'Kim', 'Guest 1');
+
+INSERT INTO user_info (login_id, first_name, last_name, display_name)
+VALUES (3, 'Micheal', 'Wong', 'Guest 2');
+
+INSERT INTO user_info (login_id, first_name, last_name, display_name)
+VALUES (4, 'Richard', 'Yang', 'Guest 3');
