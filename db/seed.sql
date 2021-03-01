16508bd35d0f47e29ce8bbf44c164aa577f308fa
Richard <richard.yang.tong@gmail.com>
2021-03-01 13:54:13 -0500

    Renamed user_info into login_info in schema.

--- db/seed.sql
+++ db/seed.sql
@@ -1,11 +1,11 @@
-INSERT INTO user_info (user_email, user_password, isAdmin) 
+INSERT INTO login_info (user_email, user_password, isAdmin) 
 VALUES ('chang.xiao@flux.com','adminPassword', true);
 
-INSERT INTO user_info (user_email, user_password) 
+INSERT INTO login_info (user_email, user_password) 
 VALUES ('sam.kim@flux.com','guessPassword1');
 
-INSERT INTO user_info (user_email, user_password) 
+INSERT INTO login_info (user_email, user_password) 
 VALUES ('micheal.wong@flux.com','guessPassword2');
 
-INSERT INTO user_info (user_email, user_password) 
+INSERT INTO login_info (user_email, user_password) 
 VALUES ('richard.yang@flux.com','guestPassword3');
