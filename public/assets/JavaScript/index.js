49dab8a0904bd5e1b998119e6cddb706664faf52
Michael Wong <michael.w228@gmail.com>
2021-03-01 15:04:32 -0500

    updated json

---
+++ public/assets/JavaScript/index.js
@@ -0,0 +1,11 @@
+//
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+
+btn.addEventListener('click', function(){
+    const userData = {
+        username: username.value,
+        password: password.value
+    }
+    
+})
