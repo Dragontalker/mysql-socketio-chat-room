80af30dfbe886f18dfb9864a0767d013372075d7
Richard <richard.yang.tong@gmail.com>
2021-03-01 13:57:19 -0500

    Added a new schema for user_info table.

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,21 @@ CREATE DATABASE flux_db;
 
 USE flux_db;
 
-CREATE TABLE user_info (
+CREATE TABLE login_info (
   id INTEGER AUTO_INCREMENT NOT NULL,
   user_email VARCHAR(255) NOT NULL,
   user_password VARCHAR(255) NOT NULL,
   access_key VARCHAR(255),
   isAdmin BOOLEAN DEFAULT false,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE user_info (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  login_id INTEGER NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  display_name VARCHAR(30),
   PRIMARY KEY(id)
 );
 
