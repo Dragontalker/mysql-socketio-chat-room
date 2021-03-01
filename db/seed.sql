50426e8da40318624a9b4deda22b1a6ee59314ab
Richard <richard.yang.tong@gmail.com>
2021-03-01 13:44:55 -0500

    Added the seeds file for MySQL database.

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,11 @@
-INSERT INTO userinfo (username, password, accessKey, isadmin) VALUES ('admin','admin','admin',1);
+INSERT INTO user_info (user_email, user_password, isAdmin) 
+VALUES ('chang.xiao@flux.com','adminPassword', true);
+
+INSERT INTO user_info (user_email, user_password) 
+VALUES ('sam.kim@flux.com','guessPassword1');
+
+INSERT INTO user_info (user_email, user_password) 
+VALUES ('micheal.wong@flux.com','guessPassword2');
+
+INSERT INTO user_info (user_email, user_password) 
+VALUES ('richard.yang@flux.com','guestPassword3');
