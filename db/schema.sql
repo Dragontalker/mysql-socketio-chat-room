51a58a6f910ac49991cc22cbdc828be5e096b24e
Sam Kim <sendtosamkim@gmail.com>
2021-03-02 10:33:01 -0500

    js and css for signup

--- db/schema.sql
+++ db/schema.sql
@@ -36,4 +36,3 @@ CREATE TABLE channels (
   channel_name VARCHAR(50),
   PRIMARY KEY(id)
 );
-
--- routes/socketRoute.js
+++ routes/socketRoute.js
@@ -1,17 +1,17 @@
 var socketIO = function (io, socket) {
-  // connect to server
-  console.log('a user connected', socket.id);
+    // connect to server
+    console.log('a user connected', socket.id);
 
-  // send/receive message
-  socket.on('message', (data) => {
-    console.log(`message from ${data.user}: ${data.msg}`);
-    io.emit('receivedMsg', { user: data.user, msg: data.msg, id: socket.id });
-  });
+    // send/receive message
+    socket.on('message', (data) => {
+        console.log(`message from ${data.user}: ${data.msg}`);
+        io.emit('receivedMsg', { user: data.user, msg: data.msg, id: socket.id });
+    });
 
-  // track disconnects
-  socket.on('disconnect', () => {
-    console.log(`user ${socket.id} disconnected`);
-  })
+    // track disconnects
+    socket.on('disconnect', () => {
+        console.log(`user ${socket.id} disconnected`);
+    })
 }
 
 module.exports = socketIO;
