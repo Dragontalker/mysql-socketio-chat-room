836f800b0abb7e6e75f03bd6ba0a80ed1e36689e
jcxiao <chang.x1994@gmail.com>
2021-03-02 11:44:51 -0500

    Added basic example code for implementing rooms in socketIO

--- routes/socketRoute.js
+++ routes/socketRoute.js
@@ -1,16 +1,35 @@
-var socketIO = function (io, socket) {
+var socketIO = function (io, socket, onlineUsers) {
   // connect to server
   console.log('a user connected', socket.id);
+  onlineUsers.push({id: socket.id, room: null});
+
+  // join rooms
+  socket.on('join', (data) => {
+    console.log(`user ${data.user} has joined room ${data.room}`);
+    socket.join(data.room);
+    // update online users list for room
+    for (let i=0; i<onlineUsers.length; i++) if (onlineUsers[i].id === data.id) onlineUsers[i].room = data.room;
+  })
 
   // send/receive message
   socket.on('message', (data) => {
-    console.log(`message from ${data.user}: ${data.msg}`);
-    io.emit('receivedMsg', { user: data.user, msg: data.msg, id: socket.id });
+    console.log(`message from room ${data.room} - ${data.user}: ${data.msg}`);
+    io.to(data.room).emit('receivedMsg', { user: data.user, msg: data.msg, id: socket.id });
+  });
+
+  // leave rooms
+  socket.on('leave', (data) => {
+    console.log (`user ${data.user} has left room ${data.room}`);
+    socket.leave(data.room);
+    // update online users list for room
+    for (let i=0; i<onlineUsers.length; i++) if (onlineUsers[i].id === data.id) onlineUsers[i].room = null;
   });
 
   // track disconnects
   socket.on('disconnect', () => {
     console.log(`user ${socket.id} disconnected`);
+    // delete user from online list
+    for (let i=0; i<onlineUsers.length; i++) if (onlineUsers[i].id === data.id) onlineUsers.splice(i,1);
   })
 }
 
