52ac8c147d61752491b05bdb605dac7c73f02992
Richard <richard.yang.tong@gmail.com>
2021-03-03 11:02:02 -0500

    Fixed a bug in findOne() orm mapping function that it did not return the table.

--- models/login_info.js
+++ models/login_info.js
@@ -1,7 +1,24 @@
 const orm = require('../config/orm');
+const db = require('../config/connection')('flux_db', process.env.SQL_PASSWORD);
 
 const login_info = {
     name: 'login_info',
+
+    getPassword: async function(userEmail) {
+        let target = 'user_password';
+        let index = `(user_email = '${userEmail}')`;
+        // console.log(`SELECT ${target} FROM ${this.name} WHERE ${index}`)
+        // let result = await db.query(`SELECT ${target} FROM ${this.name} WHERE user_email = '${userEmail}'`);
+        let result = await orm.findOne(this.name, target, index);
+        return result;
+    }
 };
+
+// Test
+const testApp = async (userEmail) => {
+    console.log(await login_info.getPassword(userEmail));
+};
+
+testApp('chang.xiao@flux.com');
 
 module.exports = login_info;
