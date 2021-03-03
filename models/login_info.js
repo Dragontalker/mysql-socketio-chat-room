15fa62579ece3b224eadb3ca0f8d580dd37b61b1
Richard <richard.yang.tong@gmail.com>
2021-03-03 11:08:07 -0500

    Added a new method matchPassword() to login_info data model.

--- models/login_info.js
+++ models/login_info.js
@@ -1,24 +1,14 @@
 const orm = require('../config/orm');
-const db = require('../config/connection')('flux_db', process.env.SQL_PASSWORD);
 
 const login_info = {
     name: 'login_info',
 
-    getPassword: async function(userEmail) {
+    matchPassword: async function(userEmail, inputPassword) {
         let target = 'user_password';
         let index = `(user_email = '${userEmail}')`;
-        // console.log(`SELECT ${target} FROM ${this.name} WHERE ${index}`)
-        // let result = await db.query(`SELECT ${target} FROM ${this.name} WHERE user_email = '${userEmail}'`);
         let result = await orm.findOne(this.name, target, index);
-        return result;
+        return result[0].user_password === inputPassword;
     }
 };
-
-// Test
-const testApp = async (userEmail) => {
-    console.log(await login_info.getPassword(userEmail));
-};
-
-testApp('chang.xiao@flux.com');
 
 module.exports = login_info;
