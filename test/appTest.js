99633cbca4af4ffb5b65590af2b091820fc99b54
Richard <richard.yang.tong@gmail.com>
2021-03-03 22:20:59 -0500

    Added an example 3-phase test.

--- test/appTest.js
+++ test/appTest.js
@@ -23,3 +23,26 @@ describe('Test 3', function() {
         assert.typeOf(result, 'string');
     });
 });
+
+// Native vs SEV
+// Naive approach
+describe('.pop', () => {
+    it('returns the last element in the array [naive]', () => {
+      assert.ok(['padawan', 'knight'].pop() === 'knight'); 
+    });
+  });
+  
+// 3 phase approach
+describe('.pop', () => {
+it('returns the last element in the array [3phase]', () => {
+    // Setup
+    const knightString = 'knight';
+    const jediPath = ['padawan', knightString];
+
+    // Exercise
+    const popped = jediPath.pop();
+
+    // Verify
+    assert.ok(popped === knightString);
+});
+});
