dd946767d07af82716c487554128fe20220eba40
Richard <richard.yang.tong@gmail.com>
2021-03-03 22:25:44 -0500

    Added an example test with teardown phase.

--- test/appTest.js
+++ test/appTest.js
@@ -46,3 +46,26 @@ it('returns the last element in the array [3phase]', () => {
     assert.ok(popped === knightString);
 });
 });
+
+// Teardown phase
+const assert = require('assert');
+const fs = require('fs');
+
+describe('appendFileSync', () => {
+  it('writes a string to text file at given path name', () => {
+
+    // Setup
+    const path = './message.txt';
+    const str = 'Hello Node.js';
+    
+    // Exercise: write to file
+    fs.appendFileSync(path, str);
+
+    // Verify: compare file contents to string
+    const contents = fs.readFileSync(path);
+    assert.ok(contents.toString() === str);
+
+    // Teardown: delete path
+    fs.unlinkSync(path);
+  });
+});
