9e91cae40a5cccd3ecac481d516201a4953784e9
Michael Wong <michael.w228@gmail.com>
2021-03-05 15:21:34 -0500

    changed button position

--- public/assets/JavaScript/chatroom.js
+++ public/assets/JavaScript/chatroom.js
@@ -34,13 +34,13 @@ async function roomList() {
   // print rooms to room list
   for (let i = 0; i < rooms.length; i++) {
     document.querySelector('#roomList').innerHTML +=
-      `<li><button class="btn btn-color chatroomBtn" id="room-${rooms[i].id}">${rooms[i].room_name}</button>
-      <button class="btn btn-outline-danger chatroomBtnDelete" id="overlayRoom">X</button></li>`;
+      `<li><button class="btn btn-color chatroomBtn btnChatRoomsize" id="room-${rooms[i].id}">${rooms[i].room_name}</button>
+      </li>`;
     document.querySelector('#overlayRoomList').innerHTML +=
       `<li><button class="btn btn-info chatroomBtn" id="overlayRoom-${rooms[i].id}">${rooms[i].room_name}</button>
       <button class="btn btn-outline-danger chatroomBtnDelete" id="overlayRoom">Delete</button></li>`;
     document.querySelector('#sbRoomList').innerHTML +=
-      `<li><button class="btn btn-info chatroomBtn" id="sbRoom-${rooms[i].id}">${rooms[i].room_name}</button>
+      `<li class="center"><button class="btn btn-info chatroomBtn btnSize" id="sbRoom-${rooms[i].id}">${rooms[i].room_name}</button>
       </li>`;
   }
   // add event listeners
