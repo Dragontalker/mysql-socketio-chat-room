60dfda5252151171feb21473c1eeaf1f33eceffe
Richard <richard.yang.tong@gmail.com>
2021-03-05 10:02:24 -0500

    Fixed a bug in login_info.matchPassword().

--- models/login_info.js
+++ models/login_info.js
@@ -5,7 +5,7 @@ const login_info = {
 
     matchPassword: async function(userName, inputPassword) {
         let target = 'user_password';
-        let index = `(user_email = '${userName}')`;
+        let index = `(user_name = '${userName}')`;
         let result = await orm.findOne(this.name, target, index);
         return result[0].user_password === inputPassword;
     },
