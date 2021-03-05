6c20e2da751d6106940279710166d11095249390
Michael Wong <michael.w228@gmail.com>
2021-03-04 22:16:33 -0500

    Updated chatroom styling

--- public/assets/JavaScript/chatroom.js
+++ public/assets/JavaScript/chatroom.js
@@ -33,7 +33,7 @@ async function roomList() {
   // print rooms to room list
   for (let i = 0; i < rooms.length; i++) {
     document.querySelector('#roomList').innerHTML +=
-      `<li><button class="btn btn-outline-info chatroomBtn" id="room-${rooms[i].id}">${rooms[i].room_name}</button>
+      `<li><button class="btn btn-color chatroomBtn" id="room-${rooms[i].id}">${rooms[i].room_name}</button>
       <button class="btn btn-outline-danger chatroomBtnDelete" id="overlayRoom">X</button></li>`;
     document.querySelector('#overlayRoomList').innerHTML +=
       `<li><button class="btn btn-info chatroomBtn" id="overlayRoom-${rooms[i].id}">${rooms[i].room_name}</button>
